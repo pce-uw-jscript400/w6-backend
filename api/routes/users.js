e8af3681a3afb6ade432016d3485ec857291aae9
rubini124 <rubinid@uw.edu>
2019-08-19 22:31:33 -0700

    Completed lesson notes

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,38 +1,66 @@
-const router = require('express').Router()
-const User = require('../models/user')
-const { isLoggedIn, isSameUser } = require('../middleware/auth')
-const { validate } = require('../middleware/users')
+const router = require("express").Router();
+const User = require("../models/user");
+const { isLoggedIn, isSameUser } = require("../middleware/auth");
+const { validate } = require("../middleware/users");
 
-const excludeKeys = '-__v -password'
+const excludeKeys = "-__v -password";
 
-router.get('/', isLoggedIn, async (req, res, next) => {
-  const status = 200
-  const response = await User.find(req.query).select(excludeKeys)
-  res.json({ status, response })
-})
+router.get("/", isLoggedIn, async (req, res, next) => {
+  const status = 200;
+  const response = await User.find(req.query).select(excludeKeys);
+  res.json({ status, response });
+});
 
-router.get('/:userId', isLoggedIn, async (req, res, next) => {
-  const status = 200
-  const response = await User.findById(req.params.userId).select(excludeKeys)
-  res.json({ status, response })
-})
+router.get("/:userId", isLoggedIn, async (req, res, next) => {
+  const status = 200;
+  const response = await User.findById(req.params.userId).select(excludeKeys);
+  res.json({ status, response });
+});
 
-router.put('/:userId', isLoggedIn, isSameUser, validate, async (req, res, next) => {
-  const status = 200
-  const query = { _id: req.params.userId }
-  const options = { new: true }
-  const response = await User.findOneAndUpdate(query, req.body, options).select(excludeKeys)
+router.put(
+  "/:userId",
+  isLoggedIn,
+  isSameUser,
+  validate,
+  async (req, res, next) => {
+    const status = 200;
+    const query = { _id: req.params.userId };
+    const options = { new: true };
+    const response = await User.findOneAndUpdate(
+      query,
+      req.body,
+      options
+    ).select(excludeKeys);
 
-  res.json({ status, response })
-})
+    res.json({ status, response });
+  }
+);
 
-router.delete('/:userId', isLoggedIn, isSameUser, async (req, res, next) => {
-  const status = 200
+router.delete("/:userId", isLoggedIn, isSameUser, async (req, res, next) => {
+  const status = 200;
 
-  const query = { _id: req.params.userId }
-  const response = await User.findOneAndDelete(query, req.body).select(excludeKeys)
+  const query = { _id: req.params.userId };
+  const response = await User.findOneAndDelete(query, req.body).select(
+    excludeKeys
+  );
 
-  res.json({ status, response })
-})
+  res.json({ status, response });
+});
 
-module.exports = router
+router.delete(
+  "/:userId/posts/:postId",
+  isLoggedIn,
+  isSameUser,
+  async (req, res, next) => {
+    const status = 200;
+    const query = { _id: req.params.userId };
+    const user = await User.findOne(query);
+    const post = user.posts.id(req.params.postId);
+    post.remove();
+    await user.save();
+
+    res.json({ status, response: post });
+  }
+);
+
+module.exports = router;
